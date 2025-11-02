15f14a73081987fe3fba48effffe144a840ceb51
Ari-Arina <arianai.zambranor@gmail.com>
2025-11-02 16:26:07 -0500

    Tested writing to a file functionality
    Next step: compare 2 files and return boolean.

--- main.cpp
+++ main.cpp
@@ -6,17 +6,41 @@ using namespace std;
 int main() {
     cout << "Start of program" << endl;
 
-    string line;
-
     ifstream inputFile("input.txt");
     if (!inputFile.is_open()) {
         cerr << "Failed to open input.txt" << endl;
         return 1;
     }
 
-    // use the correct lowercase 'getline' from <string>
-    getline(inputFile, line);
-    cout << line << endl;
+    int arr[4];
+    int index = 0;
+
+    while (inputFile >> arr[index]) {
+        index++; 
+    }
+
+    inputFile.close();
+
+    ofstream outputFile("output.txt");
+    
+    for (int i = 0; i < arr[0]; i++) {
+        int depth = arr[i+1];
+        int minutes = 0;
+        int result = 60;
+        
+        minutes += (depth/10)*2;
+        minutes += (depth-30)/10;
+
+        result -= minutes+1;
+
+        if (result < 0) {
+            result = 0;
+        }
+
+        outputFile << result << " minute(s) at " << depth << " feet" << endl;
+    }
+
+    outputFile.close();
 
     return 0;
 }
