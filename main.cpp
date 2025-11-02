b1da42b873edbf43ba62a42dbf2a4b7dded2083e
Ari-Arina <arianai.zambranor@gmail.com>
2025-11-02 15:34:47 -0500

    Tested the input reading capabilities, fixed compiler/run again

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
+    cout << "Start of program" << endl;
+
+    string line;
+
+    ifstream inputFile("input.txt");
+    if (!inputFile.is_open()) {
+        cerr << "Failed to open input.txt" << endl;
+        return 1;
+    }
+
+    // use the correct lowercase 'getline' from <string>
+    getline(inputFile, line);
+    cout << line << endl;
+
     return 0;
 }
